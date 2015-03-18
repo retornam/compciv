c184d13b893e3f6b1b17941e7588bb39bed53613
retornam <retornam@users.noreply.github.com>
2015-03-18 09:52:51 -0500

    White House Briefings Exercise 1

---
+++ homework/wh-briefings/download-briefings.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# This script  visits the  https://whitehouse.gov
+# and downloads all the available press briefings
+# page links on the  website as of March 17, 2015
+
+
+
+base_url=https://www.whitehouse.gov/briefing-room/press-briefings
+
+
+#set the last page number 
+last_num=139
+
+
+for i in $(seq  0 $last_num)
+do
+    echo "$base_url?page=$i"
+
+    curl  "$base_url?page=$i" -o "./html/$i.html"
+done
