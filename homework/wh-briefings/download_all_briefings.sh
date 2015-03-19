abe0dec44b2a085adfbfbaeae1769c950b7b8a63
retornam <retornam@users.noreply.github.com>
2015-03-19 00:07:14 -0500

    White House Press Briefings Exercise 2 second iteration

--- homework/wh-briefings/download_all_briefings.sh
+++ homework/wh-briefings/download_all_briefings.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
-
+set -o history
+set -H
 
 # This script selects all links found in the file html/all_briefing_links a
 # and downloads them to the folder html/briefings for later analysis. The
 # script assumes that the html folder is in the current working directory
 
 cwd=$(pwd)
+html_briefings_dir="$cwd/html/briefings"
+all_briefing_links="$cwd/html/all_briefing_links"
+cd $html_briefings_dir
+
+#get list of files in html_briefings_dir
+filescount=$(ls -l | wc -l)
+
+
 download_all_briefings(){
-    if [ -e './html/all_briefing_links'] ; then
+    if [ -e $all_briefing_links ] ; then
         while read p; do
-        cd ./html/briefings
-        curl  -O $p
-    done <./html/all_briefing_links
+            curl -O $p
+        done <$all_briefing_links
     else
         echo "The file all_briefing_links does not exist. Exiting.."
         exit 1;
@@ -19,14 +27,25 @@ download_all_briefings(){
 }
 
 strip_text_from_html() {
-    cd  ./html/briefings
-    for  filename in $(ls )
+    for  filename in $(ls)
     do
         cat $filename | pup 'div#content p text{}' >> all_press_briefing_words
     done
+    echo "\n"
+    echo "Printing last 10 lines of all_press_briefing_words file ...\n"
     tail -10 all_press_briefing_words
-    cd $cwd
+    echo "DONE....Exiting\n"
 }
 
+
+
+if  [ $filescount > 0 ] ; then
+    echo "We have $filescount  files in $html_briefings_dir"
+    echo "Skipping new downloads\n"
+else
+    echo "We have $filescount files in $html_briefings_dir"
+    echo "Starting new download..."
+    download_all_briefings
+fi
 
 strip_text_from_html
